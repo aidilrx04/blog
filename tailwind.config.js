df59322d67d31c82693627e3759b5a7d0ba7437a
aidilzohl <aidil@zohlgroup.com>
2024-11-18 00:44:29 +0800

    add custom tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                background: "#171821",
+                surface: "#21222d",
+                default: "#87888c",
+                heading: "#ffffff",
+                primary: "#a9dfd8",
             },
         },
     },
