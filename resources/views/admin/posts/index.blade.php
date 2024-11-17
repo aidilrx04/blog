6abdffeac6cd5b67568375dbb972812441a44066
aidilzohl <aidil@zohlgroup.com>
2024-11-18 01:52:01 +0800

    add posts index page style

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,14 +1,23 @@
-<x-layouts.admin>
-    <main class="p-3">
-        <h1>Posts</h1>
+<x-layouts.admin title="Posts | Aidil">
+    <div class="rounded-xl bg-surface p-6">
+        <h1 class="text-white mb-4 text-2xl font-medium">Posts</h1>
 
-        <ul>
+        <ul class="grid grid-cols-3 gap-6">
             @foreach ($posts as $post)
                 <li>
-                    <a href="{{ route('view_post', $post->slug) }}">{{ $post->title }}</a>
+                    <a
+                        class="flex aspect-square h-full w-full flex-col overflow-hidden rounded-xl bg-background shadow-sm transition-all hover:-translate-x-2 hover:-translate-y-2 hover:shadow-[4px_4px_1px_theme(colors.primary)]"
+                        href="{{ route("view_post", $post->slug) }}"
+                    >
+                        <div class="img flex-1 bg-background"></div>
+                        <div class="text-white p-4 font-medium capitalize">
+                            <span>
+                                {{ $post->title }}
+                            </span>
+                        </div>
+                    </a>
                 </li>
             @endforeach
         </ul>
-    </main>
-
+    </div>
 </x-layouts.admin>
