910c8e3385d01af4b780c2694a0310682e13bf1a
aidilzohl <aidil@zohlgroup.com>
2024-11-19 02:54:43 +0800

    seed a post with base tags for styling

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class PostSeeder extends Seeder
 {
@@ -14,6 +15,13 @@ class PostSeeder extends Seeder
     public function run(): void
     {
         $amount = 5;
+
+        // create base post with each tag for styling
+        $base_file = File::get(storage_path("app/private/post_base.html"));
+
+        Post::factory()->create([
+            'content' => $base_file
+        ]);
 
         Post::factory()->count($amount)->create();
     }
