f7a61d72f39bb0bd966627354f854590234e8d84
aidilzohl <aidil@zohlgroup.com>
2024-11-19 02:46:13 +0800

    display html content

--- resources/views/posts/view.blade.php
+++ resources/views/posts/view.blade.php
@@ -1,9 +1,9 @@
 <x-layouts.base>
     <main class="mx-auto w-[600px] max-w-full p-4 pt-24">
-        <h1 class="text-white mb-8 text-4xl font-medium capitalize">
+        <h1 class="mb-8 text-4xl font-medium capitalize text-white">
             {{ $post->title }}
         </h1>
 
-        <p class="">{{ $post->content }}</p>
+        <p class="">{!! $post->content !!}</p>
     </main>
 </x-layouts.base>
