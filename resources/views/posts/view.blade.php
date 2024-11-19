1d3536a405417859188ba0b231d969d72da7a3b7
aidilzohl <aidil@zohlgroup.com>
2024-11-20 01:44:11 +0800

    apply tailwind typography

--- resources/views/posts/view.blade.php
+++ resources/views/posts/view.blade.php
@@ -1,9 +1,17 @@
 <x-layouts.base>
-    <main class="mx-auto w-[600px] max-w-full p-4 pt-24">
+    <main
+        class="mx-auto w-full max-w-full p-4 pt-24 transition-all md:w-[600px] lg:w-[800px]"
+    >
         <h1 class="mb-8 text-4xl font-medium capitalize text-white">
             {{ $post->title }}
         </h1>
 
-        <p class="">{!! $post->content !!}</p>
+        <article
+            class="prose prose-xl prose-invert prose-a:text-primary prose-a:decoration-primary prose-li:marker:text-primary prose-strong:text-primary prose-green"
+        >
+            <img src="{{ $post->image }}" alt="" />
+
+            {!! $post->content !!}
+        </article>
     </main>
 </x-layouts.base>
