7e0568bdef106ee9907c68614892f9ae88047ee3
aidilzohl <aidil@zohlgroup.com>
2024-11-23 02:35:59 +0800

    change image as upload record and connect instead

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Post extends Model
 {
@@ -15,7 +16,16 @@ class Post extends Model
         'title',
         'slug',
         'content',
-        'image',
+        'image_id',
         'excerpt'
     ];
+
+    protected $with = [
+        'image'
+    ];
+
+    public function image(): HasOne
+    {
+        return $this->hasOne(Upload::class, 'image_id');
+    }
 }
