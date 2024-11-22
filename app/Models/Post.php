46b5a58f757feba6fcd5450c8442a989503612f7
aidilzohl <aidil@zohlgroup.com>
2024-11-23 03:09:43 +0800

    change image relation type from has to belongto

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Post extends Model
@@ -24,8 +25,8 @@ class Post extends Model
         'image'
     ];
 
-    public function image(): HasOne
+    public function image(): BelongsTo
     {
-        return $this->hasOne(Upload::class, 'image_id');
+        return $this->belongsTo(Upload::class, 'image_id');
     }
 }
