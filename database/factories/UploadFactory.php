76b7fa9cef8f6eee05a30543cf5146e5ec1f01f5
aidilzohl <aidil@zohlgroup.com>
2024-11-23 03:05:55 +0800

    create upload factory

---
+++ database/factories/UploadFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Upload>
+ */
+class UploadFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => 'sample-image.jpg',
+            'file' => 'sample-image.jpg',
+        ];
+    }
+}
