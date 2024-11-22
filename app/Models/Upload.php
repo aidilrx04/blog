a054d5a9fb669e07d2842e78719b2b9585246099
aidilzohl <aidil@zohlgroup.com>
2024-11-23 03:06:50 +0800

    add has factory trait

--- app/Models/Upload.php
+++ app/Models/Upload.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 
 class Upload extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'name',
         'file'
