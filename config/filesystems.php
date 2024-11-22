7fa66046021298ed6bceb11b8f7f3d592ef9d17e
aidilzohl <aidil@zohlgroup.com>
2024-11-23 02:25:43 +0800

    add uploads disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,8 +40,16 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
+            'throw' => false,
+        ],
+
+        'uploads' => [
+            'driver' => 'local',
+            'root' => storage_path('app/private/uploads'),
+            'url' => env('APP_URL') . '/uploads',
+            'visibility' => 'private',
             'throw' => false,
         ],
 
