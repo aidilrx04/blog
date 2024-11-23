c155cd5605b3e0260fd833455817f39d89e4e93b
aidilzohl <aidil@zohlgroup.com>
2024-11-24 01:45:06 +0800

    display publish date at post view

--- resources/views/posts/view.blade.php
+++ resources/views/posts/view.blade.php
@@ -1,14 +1,24 @@
 @php
+    use Carbon\Carbon;
+
     $image = $post->image;
+    $published_date = Carbon::parse($post->updated_at)->format("d M Y");
 @endphp
 
-<x-layouts.base>
+<x-layouts.front>
     <main
-        class="mx-auto w-full max-w-full p-4 pt-24 transition-all md:w-[600px] lg:w-[800px]"
+        class="mx-auto w-full max-w-full p-4 transition-all md:w-[600px] lg:w-[800px]"
     >
-        <h1 class="mb-8 text-4xl font-medium capitalize text-white">
+        <h1 class="mb-2 text-4xl font-medium capitalize text-white">
             {{ $post->title }}
         </h1>
+
+        <div class="post-data mb-4 flex items-center justify-start">
+            <span class="flex items-center gap-2">
+                <i class="ph-bold ph-calendar-dots"></i>
+                <span>{{ $published_date }}</span>
+            </span>
+        </div>
 
         <article
             class="prose prose-xl prose-green prose-invert prose-a:text-primary prose-a:decoration-primary prose-strong:text-primary prose-li:marker:text-primary"
@@ -20,4 +30,4 @@
             {!! $post->content !!}
         </article>
     </main>
-</x-layouts.base>
+</x-layouts.front>
