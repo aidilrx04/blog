93e99f390d534917f78bb2c84ca0e65796f19b7a
aidilzohl <aidil@zohlgroup.com>
2024-11-24 01:41:49 +0800

    add front end layout

---
+++ resources/views/components/layouts/front.blade.php
@@ -0,0 +1,45 @@
+@props([
+    "title" => null,
+    "head" => null,
+])
+
+<x-layouts.base {{ $attributes }}>
+    {{-- Navbar --}}
+
+    <header class="sticky left-0 top-0 z-10 h-[96px] bg-background">
+        <div
+            class="container mx-auto flex items-center justify-start gap-4 px-4"
+        >
+            <a
+                href="{{ route("index") }}"
+                class="py-4 text-4xl font-black text-primary"
+            >
+                Aidil
+            </a>
+            <nav>
+                <ul
+                    class="flex items-center gap-4 text-xl font-medium uppercase tracking-wider text-white"
+                >
+                    <li class="px-4">
+                        <a
+                            href="{{ route("index") }}"
+                            class="block rounded-xl p-4 text-primary underline underline-offset-8 hover:text-primary hover:underline"
+                        >
+                            Posts
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#project"
+                            class="block underline-offset-8 hover:text-primary hover:underline"
+                        >
+                            Project
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    {{ $slot }}
+</x-layouts.base>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,44 +2,7 @@
     use Carbon\Carbon;
 @endphp
 
-<x-layouts.base>
-    {{-- Navbar --}}
-
-    <header class="sticky left-0 top-0 z-10 h-[96px] bg-background">
-        <div
-            class="container mx-auto flex items-center justify-start gap-4 px-4"
-        >
-            <a
-                href="{{ route("index") }}"
-                class="py-4 text-4xl font-black text-primary"
-            >
-                Aidil
-            </a>
-            <nav>
-                <ul
-                    class="flex items-center gap-4 text-xl font-medium uppercase tracking-wider text-white"
-                >
-                    <li class="px-4">
-                        <a
-                            href="{{ route("index") }}"
-                            class="block rounded-xl p-4 text-primary underline underline-offset-8 hover:text-primary hover:underline"
-                        >
-                            Posts
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#project"
-                            class="block underline-offset-8 hover:text-primary hover:underline"
-                        >
-                            Project
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-
+<x-layouts.front>
     <main class="container mx-auto">
         <div class="posts rounded-xl bg-surface p-4">
             <h2 class="mb-8 text-2xl font-medium text-white">Latest Posts</h2>
@@ -79,4 +42,4 @@
             </ul>
         </div>
     </main>
-</x-layouts.base>
+</x-layouts.front>
