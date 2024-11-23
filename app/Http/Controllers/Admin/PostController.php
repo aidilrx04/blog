fb6d27c4b8c2e1cccde15fcbea2abf7742e929cc
aidilzohl <aidil@zohlgroup.com>
2024-11-24 01:59:54 +0800

    add delete post

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -98,8 +98,10 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return redirect()->route('admin.posts.index');
     }
 }
