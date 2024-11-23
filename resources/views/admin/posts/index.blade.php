d48b794548005f145a1faf298518b20ee451524a
aidilzohl <aidil@zohlgroup.com>
2024-11-24 01:57:33 +0800

    add delete button

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -6,7 +6,7 @@
             @foreach ($posts as $post)
                 <li>
                     <a
-                        class="flex aspect-square h-full w-full flex-col overflow-hidden rounded-xl bg-background shadow-sm transition-all hover:-translate-x-2 hover:-translate-y-2 hover:shadow-[4px_4px_1px_theme(colors.primary)]"
+                        class="group relative flex aspect-square h-full w-full flex-col overflow-hidden rounded-xl bg-background shadow-sm transition-all hover:-translate-x-2 hover:-translate-y-2 hover:shadow-[4px_4px_1px_theme(colors.primary)]"
                         href="{{ route("admin.posts.edit", $post->slug) }}"
                     >
                         <div class="img flex-1 overflow-hidden bg-background">
@@ -20,6 +20,24 @@
                             <span>
                                 {{ empty(trim($post->title)) === false ? $post->title : "(no title)" }}
                             </span>
+                        </div>
+                        <div
+                            class="absolute right-0 top-0 hidden group-hover:block"
+                        >
+                            <form
+                                action="{{ route("admin.posts.destroy", $post->id) }}"
+                                method="POST"
+                            >
+                                @csrf
+                                @method("DELETE")
+
+                                <button
+                                    type="submit"
+                                    class="flex size-12 items-center justify-center rounded-bl-xl bg-red-500 text-white"
+                                >
+                                    <i class="ph ph-trash text-xl"></i>
+                                </button>
+                            </form>
                         </div>
                     </a>
                 </li>
