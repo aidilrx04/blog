25c7766f699dc3b4f332f6b92a8e6372b367c1fb
aidilzohl <aidil@zohlgroup.com>
2024-11-24 01:05:43 +0800

    display only published posts

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -10,7 +10,7 @@ class RouteController extends Controller
     public function index()
     {
 
-        $posts = Post::orderBy('created_at', 'desc')->get();
+        $posts = Post::orderBy('created_at', 'desc')->orWhere('status', 'PUBLISHED')->get();
 
         return view('index', compact('posts'));
     }
