6c07a995ab3a595a1fb2139392e48fd236f23b05
aidilzohl <aidil@zohlgroup.com>
2024-11-24 01:52:12 +0800

    add post image to admin inde

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -9,7 +9,13 @@
                         class="flex aspect-square h-full w-full flex-col overflow-hidden rounded-xl bg-background shadow-sm transition-all hover:-translate-x-2 hover:-translate-y-2 hover:shadow-[4px_4px_1px_theme(colors.primary)]"
                         href="{{ route("admin.posts.edit", $post->slug) }}"
                     >
-                        <div class="img flex-1 bg-background"></div>
+                        <div class="img flex-1 overflow-hidden bg-background">
+                            <img
+                                src="{{ route("uploads.view", optional($post->image)->name ?? "sample-image.jpg") }}"
+                                alt=""
+                                class="size-full max-h-full max-w-full object-cover"
+                            />
+                        </div>
                         <div class="p-4 font-medium capitalize text-white">
                             <span>
                                 {{ empty(trim($post->title)) === false ? $post->title : "(no title)" }}
