e96f1ae1bfb4a40f78fe9103aea4712e03957fa8
aidilrx04 <mrcrayv14@gmail.com>
2024-11-29 12:03:17 +0800

    create register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,13 +1,58 @@
 <x-layouts.front>
-    <main class="flex h-full min-h-screen items-center justify-center">
-        <div class="rounded-xl bg-surface p-4">
-			<form action="">
-				<h1>Register</h1>
+    <main class="flex h-full min-h-screen items-center justify-center p-4">
+        <div class="-mt-28 w-[450px] rounded-xl bg-surface p-4">
+            <form action="{{ route("auth.register_submit") }}" method="POST">
+                @csrf
 
-				<div class="input-group">
-					
-				</div>
-			</form>
-		</div>
+                <h1 class="mb-4 text-2xl font-medium text-white">Register</h1>
+
+                <label for="name" class="mb-4 block">
+                    <span class="mb-2 block font-medium">Name</span>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        class="form-input block w-full rounded bg-background/50"
+                    />
+                </label>
+
+                <label for="email" class="mb-4 block">
+                    <span class="mb-2 block font-medium">Email</span>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        class="form-input block w-full rounded bg-background/50"
+                    />
+                </label>
+
+                <label for="password" class="mb-4 block">
+                    <span class="mb-2 block font-medium">Password</span>
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        class="form-input block w-full rounded bg-background/50"
+                    />
+                </label>
+
+                <label for="password_confirmation" class="mb-4 block">
+                    <span class="mb-2 block font-medium">Confirm Password</span>
+                    <input
+                        type="password"
+                        name="password_confirmation"
+                        id="password_confirmation"
+                        class="form-input block w-full rounded bg-background/50"
+                    />
+                </label>
+
+                <button
+                    type="submit"
+                    class="block w-full rounded bg-primary px-4 py-2 text-center font-medium text-gray-900"
+                >
+                    Register
+                </button>
+            </form>
+        </div>
     </main>
 </x-layouts.front>
