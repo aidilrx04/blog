fbf037d7abcb356e37403cc23a20739a9994f66b
aidilrx04 <mrcrayv14@gmail.com>
2024-11-29 11:32:31 +0800

    Create Auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email|unique:users,email',
+            'name' => 'required|string',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        // create user
+        $user = User::create([
+            'email' => $validated['email'],
+            'name' => $validated['name'],
+            'password' => Hash::make($validated['password'])
+        ]);
+
+        // login user
+        Auth::login($user);
+
+        return redirect()->route('index');
+    }
+
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|exists:users,email',
+            'password' => 'required|string'
+        ]);
+
+        // attemp
+        if (Auth::attempt($validated)) {
+            return redirect()->route('index');
+        }
+
+        return back()->withInput()->withErrors([
+            'failed' => 'Invalid email/password'
+        ]);
+    }
+}
