bd115af261077518edbbb1d41295e0f05996a760
aidilrx04 <mrcrayv14@gmail.com>
2025-08-24 01:23:24 +0800

    update posts layout

--- resources/views/components/main/index.blade.php
+++ resources/views/components/main/index.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.main>
-    <div class="hero h-72 flex flex-col items-center justify-center -mt-8">
+    <div class="hero h-72 flex flex-col items-center justify-center -mt-8 text-center">
         <!-- <h1 class="font-semibold text-5xl"><span class="text-blue-400">Knowledge</span> for <span class="text-teal-600">All</span></h1> -->
         <h1 class="font-black text-6xl leading-tight tracking-tight text-gray-900">
             <span class="bg-gradient-to-r from-blue-400 to-blue-600 bg-clip-text text-transparent">
@@ -14,23 +14,27 @@
             Unlock learning without limits. Accessible, Inspiring, and Built for Everyone.
         </p>
     </div>
-    <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+    <ul class="grid grid-cols-1 md:grid-cols-2  gap-6">
         @foreach ($posts as $post)
         @php
         $bg_image = $post->image ?? '/assets/default-post-background.svg';
         @endphp
         <li>
-            <a
-                href="{{ route('posts.show', $post->id) }}"
-                class="block group rounded-2xl overflow-hidden shadow-md hover:shadow-xl transition-shadow duration-300 border-2 border-gray-800 hover:bg-gray-800/50">
-                <div
-                    class="w-full aspect-square flex items-end px-4 py-3 relative">
-                    <img src="{{ asset($bg_image) }}" alt="" class="size-full absolute left-0 top-0 block object-cover group-hover:brightness-75">
-                    <h2 class="relative font-semibold text-lg text-white group-hover:text-blue-400 line-clamp-2 h-[2lh] ">
+            <article>
+
+                <a
+                    href="{{ route('posts.show', $post->id) }}"
+                    class="block group">
+                    <div
+                        class="w-full rounded-2xl overflow-hidden aspect-video flex items-end px-4 py-3 relative shadow-md hover:shadow-xl transition-shadow duration-300 border-2 border-gray-800 hover:bg-gray-800/50">
+                        <img src="{{ asset($bg_image) }}" alt="" class="size-full absolute left-0 top-0 block object-cover group-hover:brightness-75">
+
+                    </div>
+                    <h2 class="font-semibold box-content text-lg text-white group-hover:text-blue-400 line-clamp-2 my-3.5 ">
                         {{ $post->title }}
                     </h2>
-                </div>
-            </a>
+                </a>
+            </article>
         </li>
         @endforeach
     </ul>
