f9d8615936240cfa77d043f47829476272216615
aidilrx04 <mrcrayv14@gmail.com>
2025-08-31 14:50:11 +0800

    add slug after title

--- database/migrations/2025_08_31_063552_add_slug_column_to_posts_table.php
+++ database/migrations/2025_08_31_063552_add_slug_column_to_posts_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->string('slug');
+            $table->string('slug')
+                ->after('title')->unique();
         });
     }
 
